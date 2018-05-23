8f5b42d0f08f1230e422f65f28842cde13eca414
Charles <charlezwang@gmail.com>
2018-05-23 12:23:54 -0700

    mainmenuupdate

--- Bomberman-May21/src/MainMenu.java
+++ Bomberman-May21/src/MainMenu.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,21 +12,44 @@ public class MainMenu extends JFrame implements ActionListener
 	
 	public MainMenu()
 	{
-		super("Bomberman");
+		super("Bomberbitch");
 		setSize(1280, 720);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationByPlatform(true);
 		
 		startButton = new JButton("Start!");
+		startButton.addActionListener(e -> this.dispose());
 		howToPlayButton = new JButton("How to Play");
+		howToPlayButton.addActionListener(e -> this.dispose());
 		quitButton = new JButton("Quit");
+		quitButton.addActionListener(e -> this.dispose());
+		
+		this.setLayout(new BorderLayout());
+		this.add(startButton, BorderLayout.NORTH);
+		this.add(howToPlayButton, BorderLayout.SOUTH);
+		this.add(quitButton, BorderLayout.CENTER);
+		
 		
 		setVisible(true);
 	}
 	
 	public void actionPerformed(ActionEvent event)
 	{
+		if (event.getSource() == startButton){
+			InGame game = new InGame();
+			System.exit(0);
+		}
+		if (event.getSource() == howToPlayButton){
+			Instructions instruc = new Instructions();
+			//open instructions
+		}
+		if (event.getSource() == quitButton){
+			this.dispose();
+		}
 		
+	}
+	public static void main(String[] args){
+		MainMenu meme = new MainMenu();
 	}
 }
