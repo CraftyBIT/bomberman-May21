43a611352ab281a5b5943ffa85d00bfb46f5e3bf
Cornell <trancornellh@gmail.com>
2018-05-23 11:49:35 -0700

    updated mainmenu

--- Bomberman-May21/src/MainMenu.java
+++ Bomberman-May21/src/MainMenu.java
@@ -6,12 +6,21 @@ import javax.swing.JFrame;
 
 public class MainMenu extends JFrame implements ActionListener
 {
-	private JButton startButton, howToPlayButton;
+	private JButton startButton, howToPlayButton, quitButton;
+	
 	public MainMenu()
 	{
 		super("Bomberman");
-		startButton = new JButton("Start game");
+		setSize(1280, 720);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationByPlatform(true);
+		
+		startButton = new JButton("Start!");
 		howToPlayButton = new JButton("How to Play");
+		quitButton = new JButton("Quit");
+		
+		setVisible(true);
 	}
 	
 	public void actionPerformed(ActionEvent event)
