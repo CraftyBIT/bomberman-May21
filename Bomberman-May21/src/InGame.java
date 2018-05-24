0ffdbcfea455da24183d365da4bbd0d792616abb
Cornell <trancornellh@gmail.com>
2018-05-24 11:23:10 -0700

    updated InGame GUI

--- Bomberman-May21/src/InGame.java
+++ Bomberman-May21/src/InGame.java
@@ -1,15 +1,15 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 public class InGame extends JFrame implements ActionListener
 {
 	private static final long serialVersionUID = 1L;
 	private JButton startButton, howToPlayButton, quitButton;
 	private JPanel inGame;
+	private JLabel[][] grid;
 	
 	public InGame()
 	{
@@ -19,7 +19,21 @@ public class InGame extends JFrame implements ActionListener
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationByPlatform(true);
 		
+		inGame = new JPanel(new BorderLayout());
+		grid = new JLabel[11][15];
+		for (int row = 0; row < 11; row++)
+		{
+			for (int col = 0; col < 15; col++)
+			{
+				grid[row][col] = new JLabel();
+				grid[row][col].setBorder(new LineBorder(Color.BLACK));
+				grid[row][col].setOpaque(true);
+				inGame.add(grid[row][col]);
+			}
+		}
+		grid[0][0].setBackground(Color.WHITE);
 		
+		setContentPane(inGame);
 		setVisible(true);
 	}
 	
