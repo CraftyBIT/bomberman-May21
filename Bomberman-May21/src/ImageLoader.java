aa71b9caeaf350813260a5e83dcc36e5a284e3f9
Cornell <trancornellh@gmail.com>
2018-05-31 10:22:52 -0700

    updated ImageLoader with proper images that will be used

--- Bomberman-May21/src/ImageLoader.java
+++ Bomberman-May21/src/ImageLoader.java
@@ -4,7 +4,7 @@ import javax.swing.ImageIcon;
 
 
 public class ImageLoader {
-	private ImageIcon unbreakable, stoneTile;
+	private ImageIcon unbreakable, stoneTile, unbreakable2, bomb;
 	
 	public ImageLoader() {
 		ClassLoader cldr = this.getClass().getClassLoader();
@@ -15,6 +15,12 @@ public class ImageLoader {
 			
 			URL stoneTileURL = cldr.getResource("images/stonetile.png");
 			stoneTile = new ImageIcon(stoneTileURL);
+			
+			URL unbreakable2URL = cldr.getResource("images/unbreakable_block2.png");
+			unbreakable2 = new ImageIcon(unbreakable2URL);
+			
+			URL bombURL = cldr.getResource("images/bomb.png");
+			bomb = new ImageIcon(bombURL);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -26,5 +32,15 @@ public class ImageLoader {
 	
 	public ImageIcon getStoneTileImage() {
 		return stoneTile;
+	}
+	
+	public ImageIcon getUnbreakable2Image()
+	{
+		return unbreakable2;
+	}
+	
+	public ImageIcon getBombImage()
+	{
+		return bomb;
 	}
 }
