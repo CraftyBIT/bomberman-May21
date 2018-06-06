ff42d2d57d01db493d7fe287dfac232680865583
Cornell <trancornellh@gmail.com>
2018-06-06 12:05:34 -0700

    button showing full screen mainmenu

--- Bomberman-May21/src/MainMenu.java
+++ Bomberman-May21/src/MainMenu.java
@@ -22,16 +22,21 @@ public class MainMenu extends JFrame implements ActionListener
 		
 		ImageLoader imageLoader = new ImageLoader();
 		mainMenu = imageLoader.getMainMenuImage().getImage();
-		
+
+		/*
 		menuPanel = new JPanel(new GridLayout(1, 1));
 		menuPanel.setSize(960, 732);
 		menuPanel.setOpaque(true);
 		menuPanel.setBackground(Color.WHITE);
 		menuPanel.setLayout(null);
+		*/
+		JButton backgndButton = new JButton(new ImageIcon(mainMenu));
+		add(backgndButton);
 		
 		startButton = new JButton("Start!");
-		startButton.setSize(350, 50);
-		startButton.setLocation(283, 50);
+//		startButton.setSize(350, 50);
+//		startButton.setLocation(283, 50);
+		startButton.setBounds(283, 50, 350, 50);
 		startButton.addActionListener(this);
 		
 		howToPlayButton = new JButton("How to Play");
@@ -44,19 +49,30 @@ public class MainMenu extends JFrame implements ActionListener
 		quitButton.setLocation(283, 200);
 		quitButton.addActionListener(this);
 		
+		/*
 		menuPanel.add(startButton);
 		menuPanel.add(howToPlayButton);
 		menuPanel.add(quitButton);
 		setContentPane(menuPanel);
+		*/
+		
+		backgndButton.setLayout(new GridLayout(3, 1));
+		backgndButton.add(startButton);
+		
+		backgndButton.add(howToPlayButton);
+		backgndButton.add(quitButton);
+		
 		
 		setVisible(true);
 	}
 	
+	/*
 	public void paint(Graphics g)
 	{
 		super.paint(g);
 		g.drawImage(mainMenu, 0, 0, menuPanel);
 	}
+	*/
 	
 	public void actionPerformed(ActionEvent event)
 	{
