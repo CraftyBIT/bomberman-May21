757b29d5db2a7894f3f9f6e055dde454933ded51
Cornell <trancornellh@gmail.com>
2018-06-07 10:03:33 -0700

    cleaned up

--- Bomberman-May21/src/MainMenu.java
+++ Bomberman-May21/src/MainMenu.java
@@ -32,18 +32,17 @@ public class MainMenu extends JFrame implements ActionListener
 		background.setBounds(0, 0, 960, 732);
 		
 		startButton = new JButton("Start!");
-		startButton.setSize(350, 50);
+		startButton.setSize(360, 50);
     	startButton.setLocation(283, 50);
-		startButton.setBounds(283, 50, 350, 50);
 		startButton.addActionListener(this);
 		
 		howToPlayButton = new JButton("How to Play");
-		howToPlayButton.setSize(350, 50);
+		howToPlayButton.setSize(360, 50);
 		howToPlayButton.setLocation(283, 125);
 		howToPlayButton.addActionListener(this);
 		
 		quitButton = new JButton("Quit");
-		quitButton.setSize(350, 50);
+		quitButton.setSize(360, 50);
 		quitButton.setLocation(283, 200);
 		quitButton.addActionListener(this);
 		
