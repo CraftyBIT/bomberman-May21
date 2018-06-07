3277c5070e8477deffc773cc598c4dfaee94d164
Charles <charlezwang@gmail.com>
2018-06-07 10:53:14 -0700

    run doggy

--- Bomberman-May21/src/Bomb.java
+++ Bomberman-May21/src/Bomb.java
@@ -2,7 +2,7 @@
 
 public class Bomb extends Actor
 {
-	private final int explodeTime = 30; // Bomb explodes after 3 seconds
+	private final int explodeTime = 20; // Bomb explodes after 2 seconds
 	private int bombPower, playerId, time, myRow, myColumn;
 	
 	public Bomb(int row, int column, int power, int player)
